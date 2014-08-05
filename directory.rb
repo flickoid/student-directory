569871c9fa66a223ce5d7a2fc13a1b3dde7535d3
Chris Oatley <chris.oatley@gmail.com>
2014-08-05 15:49:03 +0100

    added methods

--- directory.rb
+++ directory.rb
@@ -24,11 +24,21 @@ students = [
 	"Henry Stanley"
 ]
 
-puts "The students of my cohort at Makers Academy"
-puts "------------------------"
-
-students.each do 
-	|student| puts student
+def print_header
+	puts "The students of my cohort at Makers Academy"
+	puts "------------------------"
 end
 
-puts "Overall, we have #{students.length} great students"
+def print(names)
+	names.each do |name| 
+		puts name
+	end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{names.length} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
