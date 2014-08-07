32af1aac6852a5747fb4c12a7a6ffe7969e9d64e
flickoid <chris.oatley@gmail.com>
2014-08-07 18:36:33 +0100

    expanded student information

--- directory.rb
+++ directory.rb
@@ -1,29 +1,29 @@
-students = [
-	{:name => "Ruth Earle", :cohort => :August},
-	{:name => "Andy Gates", :cohort => :August},
-	{:name => "Marc Singh", :cohort => :August},
-	{:name => "Faisal Aydarus", :cohort => :August},
-	{:name => "Ethel Ng", :cohort => :August},
-	{:name => "Kevin Daniells", :cohort => :August},
-	{:name => "Maya Driver", :cohort => :August},
-	{:name => "Leopold Kwok", :cohort => :August},
-	{:name => "James McNeil", :cohort => :August},
-	{:name => "Jerome Pratt", :cohort => :August},
-	{:name => "David Wickes", :cohort => :August},
-	{:name => "Javier Silverio", :cohort => :August},
-	{:name => "Elliot Lewis", :cohort => :August},
-	{:name => "Ben Tillett", :cohort => :August},
-	{:name => "Vincent Koch", :cohort => :August},
-	{:name => "Michelle Ballard", :cohort => :August},
-	{:name => "Tatiana Soukiassian", :cohort => :August},
-	{:name => "Mervé Silk", :cohort => :August},
-	{:name => "Albert Vallervu", :cohort => :August},
-	{:name => "Lovis Schultze", :cohort => :August},
-	{:name => "Chris Oatley", :cohort => :August},
-	{:name => "Spike Lindsey", :cohort => :August},
-	{:name => "Nick Roberts", :cohort => :August},
-	{:name => "Henry Stanley", :cohort => :August}
-]
+# students = [
+# 	{:name => "Ruth Earle", :cohort => :August},
+# 	{:name => "Andy Gates", :cohort => :August},
+# 	{:name => "Marc Singh", :cohort => :August},
+# 	{:name => "Faisal Aydarus", :cohort => :August},
+# 	{:name => "Ethel Ng", :cohort => :August},
+# 	{:name => "Kevin Daniells", :cohort => :August},
+# 	{:name => "Maya Driver", :cohort => :August},
+# 	{:name => "Leopold Kwok", :cohort => :August},
+# 	{:name => "James McNeil", :cohort => :August},
+# 	{:name => "Jerome Pratt", :cohort => :August},
+# 	{:name => "David Wickes", :cohort => :August},
+# 	{:name => "Javier Silverio", :cohort => :August},
+# 	{:name => "Elliot Lewis", :cohort => :August},
+# 	{:name => "Ben Tillett", :cohort => :August},
+# 	{:name => "Vincent Koch", :cohort => :August},
+# 	{:name => "Michelle Ballard", :cohort => :August},
+# 	{:name => "Tatiana Soukiassian", :cohort => :August},
+# 	{:name => "Mervé Silk", :cohort => :August},
+# 	{:name => "Albert Vallervu", :cohort => :August},
+# 	{:name => "Lovis Schultze", :cohort => :August},
+# 	{:name => "Chris Oatley", :cohort => :August},
+# 	{:name => "Spike Lindsey", :cohort => :August},
+# 	{:name => "Nick Roberts", :cohort => :August},
+# 	{:name => "Henry Stanley", :cohort => :August}
+# ]
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -65,21 +65,56 @@ def print_footer(names)
 end
 
 # asks for user input for names of students, adds them to an array then a hash
-def input_students
+# def input_students
+# 	puts "Please enter the names of the students"
+# 	puts "To finish, just hit return twice"
+
+# 	students = []
+
+# 	name = gets.chomp
+
+# 	while !name.empty? do
+# 		students << {:name => name, :cohort => :August}
+# 		puts "Now we have #{students.length} students"
+
+# 		name = gets.chomp
+# 	end
+# 	students
+# end
+
+
+def input_students_multi
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
 
 	students = []
-
 	name = gets.chomp
-
 	while !name.empty? do
-		students << {:name => name, :cohort => :August}
+		students << build_student_from_input(name)
 		puts "Now we have #{students.length} students"
-
 		name = gets.chomp
 	end
-	students
+end
+
+def build_student_from_input(name)
+	
+
+	puts "Please enter their cohort"
+	cohort = gets.chomp
+
+	puts "Please enter their hobbies"
+	hobbies = gets.chomp
+
+	puts "Please enter their country of birth"
+	country = gets.chomp
+
+	puts "What is their favourite cartoon character?"
+	cartoon = gets.chomp
+	create_student(name, cohort, hobbies, country, cartoon)
+end
+
+def create_student(name, cohort, hobbies, country, cartoon)
+	{:name => name, :cohort => cohort, :hobbies => hobbies, :country => country, :cartoon => cartoon}
 end
 
 # allows you to choose a letter and will then select only the students whose names begin with that letter
@@ -94,7 +129,7 @@ def less_twelve(students)
 	students.select {|student| student[:name].length < 12}
 end
 
-# students = input_students
+students = input_students_multi
 print_header
 # print(start_with(students,"D"))
 # print selection(students)
