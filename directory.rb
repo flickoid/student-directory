2b69cb07ce147975eb0e6da6a228ebcaeb2e68aa
flickoid <chris.oatley@gmail.com>
2014-08-07 17:24:15 +0100

    Changed .each to while loop

--- directory.rb
+++ directory.rb
@@ -1,33 +1,43 @@
-# students = [
-# 	{:name => "Ruth Earle", :cohort => :August},
-# 	{:name => "Andy Gates", :cohort => :August},
-# 	{:name => "Marc Singh", :cohort => :August},
-# 	{:name => "Faisal Aydarus", :cohort => :August},
-# 	{:name => "Ethel Ng", :cohort => :August},
-# 	{:name => "Kevin Daniells", :cohort => :August},
-# 	{:name => "Maya Driver", :cohort => :August},
-# 	{:name => "Leopold Kwok", :cohort => :August},
-# 	{:name => "James McNeil", :cohort => :August},
-# 	{:name => "Jerome Pratt", :cohort => :August},
-# 	{:name => "David Wickes", :cohort => :August},
-# 	{:name => "Javier Silverio", :cohort => :August},
-# 	{:name => "Elliot Lewis", :cohort => :August},
-# 	{:name => "Ben Tillett", :cohort => :August},
-# 	{:name => "Vincent Koch", :cohort => :August},
-# 	{:name => "Michelle Ballard", :cohort => :August},
-# 	{:name => "Tatiana Soukiassian", :cohort => :August},
-# 	{:name => "Mervé Silk", :cohort => :August},
-# 	{:name => "Albert Vallervu", :cohort => :August},
-# 	{:name => "Lovis Schultze", :cohort => :August},
-# 	{:name => "Chris Oatley", :cohort => :August},
-# 	{:name => "Spike Lindsey", :cohort => :August},
-# 	{:name => "Nick Roberts", :cohort => :August},
-# 	{:name => "Henry Stanley", :cohort => :August}
-# ]
+students = [
+	{:name => "Ruth Earle", :cohort => :August},
+	{:name => "Andy Gates", :cohort => :August},
+	{:name => "Marc Singh", :cohort => :August},
+	{:name => "Faisal Aydarus", :cohort => :August},
+	{:name => "Ethel Ng", :cohort => :August},
+	{:name => "Kevin Daniells", :cohort => :August},
+	{:name => "Maya Driver", :cohort => :August},
+	{:name => "Leopold Kwok", :cohort => :August},
+	{:name => "James McNeil", :cohort => :August},
+	{:name => "Jerome Pratt", :cohort => :August},
+	{:name => "David Wickes", :cohort => :August},
+	{:name => "Javier Silverio", :cohort => :August},
+	{:name => "Elliot Lewis", :cohort => :August},
+	{:name => "Ben Tillett", :cohort => :August},
+	{:name => "Vincent Koch", :cohort => :August},
+	{:name => "Michelle Ballard", :cohort => :August},
+	{:name => "Tatiana Soukiassian", :cohort => :August},
+	{:name => "Mervé Silk", :cohort => :August},
+	{:name => "Albert Vallervu", :cohort => :August},
+	{:name => "Lovis Schultze", :cohort => :August},
+	{:name => "Chris Oatley", :cohort => :August},
+	{:name => "Spike Lindsey", :cohort => :August},
+	{:name => "Nick Roberts", :cohort => :August},
+	{:name => "Henry Stanley", :cohort => :August}
+]
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "------------------------"
+end
+
+# changes .each to while loop
+def print_loop(students)
+	length = students.length
+	count = 0
+	while count < length do 
+		puts "#{students[count][:name]}, #{students[count][:cohort]} cohort"
+		count += 1
+		end
 end
 
 def print(students)
@@ -36,6 +46,8 @@ def print(students)
 	end
 end
 
+
+# Dave's code
 # def start_with(students, start_letter)
 # 	students.select {|student| student[:name][0] == start_letter}
 # end
@@ -82,9 +94,10 @@ def less_twelve(students)
 	students.select {|student| student[:name].length < 12}
 end
 
-students = input_students
+# students = input_students
 print_header
 # print(start_with(students,"D"))
 # print selection(students)
-print less_twelve(students)
+# print less_twelve(students)
+print_loop(students)
 print_footer(students)
